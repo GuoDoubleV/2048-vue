4c3a4bc29a5ee3159b2061933d2549dbcc2fb723
guoweijun <891335563@qq.com>
2022-01-07 10:43:45 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Game></Game>
+</template>
+
+<script setup>
+import Game from "./components/Game.vue";
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background: #faf8f0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
